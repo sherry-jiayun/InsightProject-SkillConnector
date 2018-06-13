f19e604f28306dd79d143379f72867bd946aa539
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-06-13 15:15:46 -0400

    test project

---
+++ testpythonfile.py
@@ -0,0 +1,2 @@
+
+dataframe_mysql = sqlContext.read.format("jdbc").options(url="jdbc:mysql://sg-cli-test.cdq0uvoomk3h.us-east-1.rds.amazonaws.com:3306/stackoverflow2010",driver = "com.mysql.jdbc.Driver",dbtable="stackoverflow2010.posts",user="sherry_jiayun",password="yjy05050609").load()
