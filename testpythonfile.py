9ce47b971853a3279e1688cc5edc2e98b34396ed
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-06-13 15:40:00 -0400

    test 6

--- testpythonfile.py
+++ testpythonfile.py
@@ -3,4 +3,6 @@ from pyspark import SparkContext
 
 sc = SparkContext("local", "Simple App")
 sqlContext = SQLContext(sc)
-dataframe_mysql = sqlContext.read.format("jdbc").options(url="jdbc:mysql://sg-cli-test.cdq0uvoomk3h.us-east-1.rds.amazonaws.com:3306/stackoverflow2010",driver = "com.mysql.jdbc.Driver",dbtable="stackoverflow2010.posts",user="sherry_jiayun",password="yjy05050609").load()
+
+dataframe_mysql = sqlContext.read.format("jdbc").options(url="jdbc:mysql://sg-cli-test.cdq0uvoomk3h.us-east-1.rds.amazonaws.com:3306/stackoverflow2010",driver = "com.mysql.jdbc.Driver",dbtable="postlinks",user="sherry_jiayun",password="yjy05050609").load()
+dataframe_mysql.show()
