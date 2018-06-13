a7da148287b7ebc984b4aa4214420817211984d8
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-06-13 15:37:44 -0400

    test 5

--- testpythonfile.py
+++ testpythonfile.py
@@ -1,3 +1,6 @@
 from pyspark.sql import SQLContext
+from pyspark import SparkContext
+
+sc = SparkContext("local", "Simple App")
 sqlContext = SQLContext(sc)
 dataframe_mysql = sqlContext.read.format("jdbc").options(url="jdbc:mysql://sg-cli-test.cdq0uvoomk3h.us-east-1.rds.amazonaws.com:3306/stackoverflow2010",driver = "com.mysql.jdbc.Driver",dbtable="stackoverflow2010.posts",user="sherry_jiayun",password="yjy05050609").load()
