ae9f43e44e7c7cfc0768140a2d12d4198f3d2883
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-06-14 14:12:47 -0400

    parallal test 4

--- testpythonfile.py
+++ testpythonfile.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SQLContext
-from pyspark import SparkContext,parkConf
+from pyspark import SparkContext,SparkConf
 from neo4j.v1 import GraphDatabase
 from pyspark.sql.functions import *
 
