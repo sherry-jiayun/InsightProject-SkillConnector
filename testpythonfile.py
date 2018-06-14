612b72fd945ac189be9060f10e3e9b0f738452f1
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-06-14 10:00:08 -0400

    test 1

--- testpythonfile.py
+++ testpythonfile.py
@@ -4,5 +4,48 @@ from pyspark import SparkContext
 sc = SparkContext("local", "Simple App")
 sqlContext = SQLContext(sc)
 
-dataframe_mysql = sqlContext.read.format("jdbc").options(url="jdbc:mysql://sg-cli-test.cdq0uvoomk3h.us-east-1.rds.amazonaws.com:3306/stackoverflow2010",driver = "com.mysql.jdbc.Driver",dbtable="postlinks",user="sherry_jiayun",password="yjy05050609").load()
-dataframe_mysql.show()
+# connect to neo4j
+uri = "bolt://ec2-34-234-207-154.compute-1.amazonaws.com:7687"
+driver = GraphDatabase.driver(uri,auth=("neo4j","yjy05050609"))
+session = driver.session()
+
+# get null null tags from 
+df = sqlContext.read.format("jdbc").options(url="jdbc:mysql://sg-cli-test.cdq0uvoomk3h.us-east-1.rds.amazonaws.com:3306/stackoverflow2010",driver = "com.mysql.jdbc.Driver",dbtable="(SELECT AnswerCount,CommentCount,FavoriteCount,Tags FROM posts WHERE Tags IS NOT NULL LIMIT 100) tmp",user="sherry_jiayun",password="yjy05050609").load()
+# replace < > and.  
+df = df.withColumn('Tags', regexp_replace('Tags', '<', ' '))
+df = df.withColumn('Tags', regexp_replace('Tags', '>', ' '))
+df = df.withColumn('Tags', regexp_replace('Tags', '  ', ' '))
+
+# calculate weight
+# 
+df = df.withColumn('total', df['AnswerCount']+df['CommentCount']+df['FavoriteCount'])
+
+
+def get_v(xx):
+	xx_v = ''
+	for xc in xx:
+		if xc.isalpha():
+			xx_v += xc
+		else:
+			xx_v += '_'
+	return xx_v
+
+# create vertex and edge 
+# MERGE (:Vertex { Name : "C++" }) MERGE (:Vertex { Name : "winform" }) 
+# MERGE (v1:Vertex {Name:'C#'})-[r:Group]->(v2:Vertex {Name:'C++'})
+check_list = list()
+for x in df.collect():
+	vertex_list = x[3].strip().split(' ')
+	for xx in vertex_list:
+		for xxx in vertex_list:
+			if not xxx == xx:
+				cypher = ""
+				cypher += "MERGE (:vertex{ name: '"+xx+"' }) " # create node 1 if not exist
+				cypher += "MERGE (:vertex{ name: '"+xxx+"'}) " # create node 2 if not exist
+				session.run(cypher)
+				cypher = ""
+				cypher += "MATCH (v1:vertex { name:'"+xx+"' }), (v2:vertex { name:'"+xxx+"'}) "
+				cypher += "MERGE (v1)-[r:Group { weight: 0 }]->(v2) " # create relationship
+				cypher += "WITH r " # update relationship
+				cypher += "SET r.weight = r.weight + "+str(x[4])
+				session.run(cypher)
