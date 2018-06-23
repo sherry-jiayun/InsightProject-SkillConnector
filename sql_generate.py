d32031bb225a0c4c122d969c00e137bd0907aa20
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-06-23 09:50:36 -0400

    Try to read in parallal

--- sql_generate.py
+++ sql_generate.py
@@ -1,7 +1,9 @@
 num = 50663511
-num_of_p = num/50000
+num_of_p = int(num/100000)
 sql_text = "ALTER TABLE Posts PARTITION BY RANGE(Id)  ("
 for i in range(num_of_p):
-	sql_text += "PARTITION p"+str(i)+" VALUES LESS THAN ("+str(50000*(i+1))+"),"
+	sql_text += "PARTITION p"+str(i)+" VALUES LESS THAN ("+str(100000*(i+1))+"),"
 
 sql_text += "PARTITION p"+str(num_of_p+1)+" VALUES LESS THAN MAXVALUE);"
+
+print(sql_text)
