2a739d5030a2d1a24a74c309807d41b3ab983cc2
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-07-04 17:16:34 -0400

    update sh

--- command.sh
+++ command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # open pyspark in master
-pyspark --jars mysql-connector-java-8.0.11.jar --master spark://(master ip)
+pyspark --jars path/to/mysql-connector-java-8.0.11.jar --master spark://(master ip)
 # submit work with mysql 
-spark-submit --jars mysql-connector-java-8.0.11.jar --master spark://(master ip):7077 python_script.py
+spark-submit --jars path/to/mysql-connector-java-8.0.11.jar --master spark://(master ip):7077 path/to/python_script.py
