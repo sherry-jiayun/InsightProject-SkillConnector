ad30bea5861f9180c0c555f6e154a2f0430b6ad4
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-07-04 17:08:41 -0400

    change command line type

--- command.py
+++
@@ -1,4 +0,0 @@
-# open pyspark in master
-pyspark --jars mysql-connector-java-8.0.11.jar --master spark://(master ip)
-# submit work with mysql 
-spark-submit --jars mysql-connector-java-8.0.11.jar --master spark://(master ip):7077 python_script.py
